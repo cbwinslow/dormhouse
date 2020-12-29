eb152e01d70965bf05e0252d773a95ec96b3a924
Sean Fischer <seanwfischer@gmail.com>
2020-12-29 18:30:10 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name="dormouse",
     version="0.3",
+    author="Sean Fischer",
+    author_email="seanwfischer@gmail.com",
+    url="https://www.github.com/fischersean/dormouse",
+    license="GPLv3",
     description="Database for jabberwocky project",
-    packages=find_packages(),
+    packages=find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]
+    ),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "pybaseball",
+        "sqlalchemy",
+        "beautifulsoup4",
+    ],
 )
