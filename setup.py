f9059ac0f011ee625f7993e32fe3b00d890e1b83
Sean Fischer <seanwfischer@gmail.com>
2020-12-29 19:19:43 -0500

    added min python version

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     url="https://www.github.com/fischersean/dormouse",
     license="GPLv3",
     description="Database for jabberwocky project",
+    python_requires=">=3.7",
     packages=find_packages(
         exclude=["tests", "*.tests", "*.tests.*", "tests.*"]
     ),
